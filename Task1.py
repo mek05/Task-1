3304951e708a3a991f25e60f8d16a1334836d08b
Mohammed Eesa Khan <89284137+mek05@users.noreply.github.com>
2021-08-21 07:09:23 +0530

    Created python program
    This program takes input of the radius and print the area for the same and also accept filename from the user and prints the extension of that

---
+++ Task1.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+r = float(input("input the radius of the circle:"))
+A = r*r*3.14
+print("Area of the circle with radius",r,"is",A)
+
+fn = input("Enter filename:")
+f = fn.split(".")
+print("Extension of the file name is:"+f[-1])
+ 
